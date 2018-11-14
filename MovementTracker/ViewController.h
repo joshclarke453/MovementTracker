c176e310668c822c97042c97194059c0d4c5f62b
Joshua <joshuaxcode@grannysmith.cs.mun.ca>
2018-11-14 18:17:04 -0330

    Initial Commit

---
+++ MovementTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovementTracker
+//
+//  Created by Joshua on 2018-11-14.
+//  Copyright © 2018 jtc260. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
